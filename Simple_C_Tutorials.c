331a696ffbbf1af3c128b6c00faa141388b114a1
Nicholas Newman <nick.newman95@yahoo.com>
2019-10-22 14:42:23 -0600

    Add files via upload

--- Simple_C_Tutorials.c
+++ Simple_C_Tutorials.c
@@ -277,3 +277,18 @@ int main () {
 
 */ //10
 
+// --------- A simple string tutorial ---------
+
+/* //11
+
+#include <stdio.h>
+
+int main () {
+
+   char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+   printf("Greeting message: %s\n", greeting );
+   return 0;
+}
+
+*/ //11
+
