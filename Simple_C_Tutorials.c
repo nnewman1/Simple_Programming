b5f56678d30ff78a272956e2505d3c180fc5e85f
Nicholas Newman <nick.newman95@yahoo.com>
2020-03-09 23:47:10 -0600

    Update Simple_C_Tutorials.c

--- Simple_C_Tutorials.c
+++ Simple_C_Tutorials.c
@@ -3,6 +3,7 @@
 // To compile this file with the terminal the command is 'gcc [File_Name.c] -o [Compile_Name]'
 // To run the compiled file with the terminal the command is './[Compile_Name]'
 // Input and Output operations can also be performed in C++ using the C Standard Input and Output Library
+// C Standard General Utilities Library.
 
 // --------- A simple hello world tutorial ---------
 
